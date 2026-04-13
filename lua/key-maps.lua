553962da2d02bfa8abcb6b055de1c2b1ada96b08
RaviRajGhorsai <graviraj344@gmail.com>
2026-04-13 19:21:44 +0545

    keymap added to open terminal along side nvim window.

--- lua/key-maps.lua
+++ lua/key-maps.lua
@@ -1,13 +1,17 @@
 vim.g.mapleader = " "
-vim.opt.shiftwidth = 4    -- Indent by 4 spaces
-vim.opt.tabstop = 4       -- Tab key inserts 4 spaces
-vim.opt.expandtab = true  -- Convert tabs to spaces (important for Python!)
-vim.opt.softtabstop = 4   -- Backspace deletes 4 spaces
+vim.opt.shiftwidth = 4 -- Indent by 4 spaces
+vim.opt.tabstop = 4 -- Tab key inserts 4 spaces
+vim.opt.expandtab = true -- Convert tabs to spaces (important for Python!)
+vim.opt.softtabstop = 4 -- Backspace deletes 4 spaces
 vim.opt.autoindent = true -- Keep indentation from previous line
-vim.opt.number = true     -- Line Number is visible
+vim.opt.number = true -- Line Number is visible
 --vim.opt.relativenumber = true -- Relative line number is set
 vim.keymap.set("v", "<leader>y", ":'<,'>w !wl-copy<CR>", {
-  silent = true,
-  desc = "Copy selection to system clipboard (wl-copy)",
+	silent = true,
+	desc = "Copy selection to system clipboard (wl-copy)",
 })
 
+vim.keymap.set("n", "<leader>t", ":vsp | terminal<CR>", {
+	silent = true,
+	desc = "Open terminal inside nvim.",
+})
